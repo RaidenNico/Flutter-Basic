79dff2a220fe5de4750146e327ecba627010c7bd
Usuario GitLab <gitlab2040@gmail.com>
2023-01-07 21:09:53 -0500

    avance

--- mapbox_app/lib/src/views/fullscreenmap.dart
+++ mapbox_app/lib/src/views/fullscreenmap.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:http/http.dart' as http;
+import 'dart:typed_data';
 
 class FullScreenMap extends StatefulWidget {
   @override
@@ -8,21 +11,101 @@ class FullScreenMap extends StatefulWidget {
 
 class _FullScreenMapState extends State<FullScreenMap> {
   late MapboxMapController mapController;
-
+  final center = LatLng(-11.967745156683534, -76.99607756309396);
+  String selectedStyle = "mapbox://styles/raidennico/clcmn8d2j008q14pae3rbz92o";
+  final sateliteStyle = "mapbox://styles/raidennico/clcmn8d2j008q14pae3rbz92o";
+  final streetStyle = "mapbox://styles/raidennico/clcmd4o2a008o14paf96vlfl1";
+  final navigationStyle =
+      "mapbox://styles/raidennico/clcmn7xbl00b515qsyov8doir";
   void _onMapCreated(MapboxMapController controller) {
     mapController = controller;
+    _onStyleLoaded();
+  }
+
+  void _onStyleLoaded() {
+    addImageFromAsset("assetImage", "assets/custom-icon.png");
+    addImageFromUrl("networkImage", "https://via.placeholder.com/50");
+  }
+
+  /// Adds an asset image to the currently displayed style
+  Future<void> addImageFromAsset(String name, String assetName) async {
+    final ByteData bytes = await rootBundle.load(assetName);
+    final Uint8List list = bytes.buffer.asUint8List();
+    return mapController.addImage(name, list);
+  }
+
+  /// Adds a network image to the currently displayed style
+  Future<void> addImageFromUrl(String name, String url) async {
+    var response = await http.get(Uri.parse(url));
+    return mapController.addImage(name, response.bodyBytes);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MapboxMap(
-        accessToken:
-            "sk.eyJ1IjoicmFpZGVubmljbyIsImEiOiJjbGNsZmE5eG45a3ZzM25wbGo1dTEzOTl5In0.7aJixRWMU9-_QXW9nR6QcQ",
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: const CameraPosition(
-            target: LatLng(-11.967745156683534, -76.99607756309396)),
-      ),
+      body: crearMapa(),
+      floatingActionButton: botonesFlotantes(),
+    );
+  }
+
+  Column botonesFlotantes() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        //! Símbolos
+        FloatingActionButton(
+            child: Icon(Icons.add_location_alt_outlined),
+            onPressed: () {
+              mapController.addSymbol(SymbolOptions(
+                  geometry: center,
+                  //iconSize: 3,
+                  iconImage: 'networkImage',
+                  textField: 'Ubicacion creada aquí',
+                  textOffset: Offset(0, 2)));
+            }),
+
+        SizedBox(height: 5),
+
+        //! ZoomInt
+        FloatingActionButton(
+            child: Icon(Icons.zoom_in),
+            onPressed: () {
+              mapController.animateCamera(CameraUpdate.zoomIn());
+            }),
+
+        SizedBox(height: 5),
+
+        //! ZoomOut
+        FloatingActionButton(
+            child: Icon(Icons.zoom_out),
+            onPressed: () {
+              mapController.animateCamera(CameraUpdate.zoomOut());
+            }),
+
+        SizedBox(height: 5),
+
+        //! Cambiar Estilos
+        FloatingActionButton(
+            child: Icon(Icons.add_to_home_screen),
+            onPressed: () {
+              if (selectedStyle == sateliteStyle) {
+                selectedStyle == streetStyle;
+              } else {
+                selectedStyle = sateliteStyle;
+              }
+              setState(() {});
+            })
+      ],
+    );
+  }
+
+  MapboxMap crearMapa() {
+    return MapboxMap(
+      accessToken:
+          "sk.eyJ1IjoicmFpZGVubmljbyIsImEiOiJjbGNsZmE5eG45a3ZzM25wbGo1dTEzOTl5In0.7aJixRWMU9-_QXW9nR6QcQ",
+      styleString: selectedStyle,
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(target: center, zoom: 15),
     );
   }
 }
